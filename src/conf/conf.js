cda7909dc237b58c9c62034d000c18db55ead7ac
uznainrashid <147235060+uznainrashid@users.noreply.github.com>
2024-11-13 12:14:34 +0500

    conf-approach

---
+++ src/conf/conf.js
@@ -0,0 +1,14 @@
+const conf = {
+    apprriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    apprriteProjectID : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    apprriteDataBaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    apprriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    apprriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+
+
+
+
+
+export default conf
