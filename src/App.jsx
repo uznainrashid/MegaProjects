0a3fe90dd8b2c65b8c52395567178e5cf415745b
uznainrashid <147235060+uznainrashid@users.noreply.github.com>
2024-11-15 12:38:44 +0500

    ReduxToolKIT

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     <div className="w-full block">
       <Header/>
       <main>
-        {/* <Outlet/> */}
+        TODO : {/* <Outlet/> */}
       </main>
       <Footer/>
     </div>
